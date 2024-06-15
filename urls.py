065968bdac1d6562c688656932cabaaabe8ad833
NukleareNuesse <daniel.diring76@gmail.com>
2024-06-15 13:22:02 +0200

    Verlinkung mit der Homepage

--- urls.py
+++ urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.search_restaurants, name='search_restaurants'),
+   # path("", views.search_restaurants, name='search_restaurants'),
+    path('search/', views.search_results, name='search_results'),
+    path('search/', views.search_restaurants, name='search_restaurants'),
 ]
--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from SearchFilterFunctions.models import Restaurant, Cuisine
 from django.db.models import Q
@@ -24,3 +25,26 @@ def search_restaurants(request):
         'selected_cuisines': cuisines,
         'query': query,
     })
+
+def search_results(request):
+    query = request.GET.get('query')
+    cuisines = request.GET.getlist('cuisines')
+
+    filters = Q()
+
+    if query:
+        filters &= Q(name__icontains=query)
+
+    if cuisines:
+        filters &= Q(cuisines__id__in=cuisines)
+
+    results = Restaurant.objects.filter(filters).distinct()
+
+    all_cuisines = Cuisine.objects.all()  # Hier Cuisines hinzugefügt
+
+    return render(request, 'SearchFilterFunctions/search_results.html', {
+        'query': query,
+        'results': results,
+        'all_cuisines': all_cuisines,  # Hier all_cuisines übergeben
+    })
+
