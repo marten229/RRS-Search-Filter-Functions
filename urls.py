e735204575de68572bbf33b27467cad20ad1ecb6
Marten Schulz <ms@marten-schulz.de>
2024-06-16 15:57:10 +0200

    chore: Remove commented out code in urls.py

--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
    # path("", views.search_restaurants, name='search_restaurants'),
-    path('search/', views.search_results, name='search_results'),
-    path('search/', views.search_restaurants, name='search_restaurants'),
+    #path('search/', views.search_results, name='search_results'),
+    #path('search/', views.search_restaurants, name='search_restaurants'),
 ]
